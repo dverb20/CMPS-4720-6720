63afbc5d98ad883f3c91fd24b173f250dd0aa29f
Danny Verb <dverb20@users.noreply.github.com>
2017-02-01 17:14:18 -0600

    have csv reader

--- hw1/src/com/company/Main.java
+++ hw1/src/com/company/Main.java
@@ -2,11 +2,12 @@ package com.company;
 import java.util.*;
 import java.io.*;
 
-public class Machine_Learning_hw1 {
+
+public class Main {
 
     public static void main(String[] args) {
 
-        String csvFile = "/Users/dverb/desktop/iris.csv";
+        String csvFile = "/Users/dannyverb/Desktop/iris.csv";
         BufferedReader br = null;
         String line = "";
         String cvsSplitBy = ",";
@@ -19,7 +20,7 @@ public class Machine_Learning_hw1 {
                 // use comma as separator
                 String[] feature = line.split(cvsSplitBy);
 
-                System.out.println("number [code= " + feature[0] + " , name=" + country[2] + "]");
+                System.out.println("number [code= " + feature[0] + " , name=" + feature[2] + "]");
 
             }
 
@@ -41,66 +42,3 @@ public class Machine_Learning_hw1 {
     }
 }
 
-public class Model {
-    private double w0;
-    private double w1;
-    private double w2;
-    private double w3;
-    private double w4;
-
-    //initialize variables
-    public Model(double a, double b, double c, double d, double e){
-        w0 = a;
-        w1 = b;
-        w2 = c;
-        w3 = d;
-        w4 = e;
-    }
-
-    public void change0(double newVar){
-        w0 = newVar;
-    }
-
-    public void change1(double newVar){
-        w1 = newVar;
-    }
-
-    public void change2(double newVar){
-        w2 = newVar;
-    }
-
-    public void change3(double newVar){
-        w3 = newVar;
-    }
-
-    public void change4(double newVar){
-        w4 = newVar;
-    }
-
-    public int runSolve(double x, double y, double z, double w){
-        double fX = w0 + x*w1 + y*w2 + z*w3 + w*w4;
-        int answer = (int) (fX + 0.5);
-        return answer;
-    }
-
-    public double get0(){
-        return w0;
-    }
-
-    public double get1(){
-        return w1;
-    }
-
-    public double get2(){
-        return w2;
-    }
-
-    public double get3(){
-        return w3;
-    }
-
-    public double get4(){
-        return w4;
-    }
-
-}

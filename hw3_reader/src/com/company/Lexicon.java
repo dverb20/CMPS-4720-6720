c9e9037a3336de5bd746f654c711819c76ad97b8
Danny Verb <dverb20@users.noreply.github.com>
2017-04-17 19:26:54 -0500

    more perfect model

--- hw3_reader/src/com/company/Lexicon.java
+++ hw3_reader/src/com/company/Lexicon.java
@@ -8,11 +8,15 @@ public class Lexicon {
     private String header;
     private ArrayList<String> names;
     private ArrayList<Double> weights;
+    private ArrayList<Integer> appearances;
+    private double learningRate;
 
     public Lexicon(String h){
         header = h;
         names = new ArrayList<String>();
         weights = new ArrayList<Double>();
+        appearances = new ArrayList<Integer>();
+        learningRate = 0.08;
     }
 
     public boolean nameExists(String name){
@@ -26,37 +30,53 @@ public class Lexicon {
 
     public double getWeight(String name){
         int index = names.indexOf(name);
+        if(index == -1){
+            return 0.5;
+        }
         return weights.get(index);
     }
 
-    public void addFactor(String name, boolean success, double degree){
-        if(this.nameExists(name) == false){
-            names.add(name);
-            if(success == true){
-                weights.add(.01);
-            }
-            else{
-                weights.add(-.01);
-            }
+    public int getAppearances(String name){
+        int index = names.indexOf(name);
+        if(index == -1){
+            return 1;
         }
-        else{//must have success gate
-            int index = names.indexOf(name);
-            double update;
-            if(success == true){
-                update = weights.get(index) + 0.01;
-            }
-            else{
-                update = weights.get(index) - 0.01;
-            }
-            weights.set(index, update);
-        }
+        return appearances.get(index);
+    }
 
+    public void addAppearance(String name){
+        int index = names.indexOf(name);
+        appearances.set(index, (appearances.get(index)+1));
+    }
+
+    public void addFactor(String name, double margin){
+        names.add(name);
+        appearances.add(1);
+        weights.add(learningRate);
+
+    }
+
+    public void update(String name, double margin, double budget){
+        double weight = this.getWeight(name);
+        int index = names.indexOf(name);
+        double sign = 0;
+        //if(budget > 1000000000){
+            //sign = expected - margin + .3;
+        //}
+        //else {
+        sign = margin - weight;
+        double dif = Math.abs(sign);
+        //}
+        if(sign > 0) {sign = 1.0;}
+        if(sign < 0){ sign = -1.0; }
+        double update = weight + (learningRate*sign*dif);
+        weights.set(index, update);
     }
 
     public String toString(){
         String str = "";
         for(int i=0; i < this.names.size(); i++){
-            str += this.names.get(i) + " : " + this.weights.get(i) + "\n";
+            str += this.names.get(i) + " : " + this.weights.get(i) + ", " + this.appearances.get(i) + "\n";
         }
         str += this.header;
         str += " -- " + this.names.size();
--- hw3_reader/src/com/company/Main.java
+++ hw3_reader/src/com/company/Main.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.File;
 //import java.io.FileReader;
 import java.io.FileNotFoundException;
+import java.io.InterruptedIOException;
 import java.util.*;
 
 public class Main {
@@ -17,7 +18,8 @@ public class Main {
         //String cvsSplitBy = ",";
         int count = 0;
         //must change size based on knowledge of input data
-        ArrayList<String[]> data = new ArrayList<String[]>();
+        ArrayList<String[]> trainingData = new ArrayList<String[]>();
+        ArrayList<String[]> testData = new ArrayList<String[]>();
         String[] lexiconHeaders = {"director_name", "duration", "actors", "content_rating"};
         //duration will be grouped into blocks of 30 minutes
         //actors encompasses actor_#_name from 1-3
@@ -39,36 +41,119 @@ public class Main {
             String nextLine = scanner.nextLine();
             String[] arr=nextLine.split(",");
             if(dataGate(arr, budgetIndex, grossIndex) == true){
-                boolean success = successGate(Double.parseDouble(arr[budgetIndex]), Double.parseDouble(arr[grossIndex]));
-                //boolean success = true;
+                if(Integer.parseInt(arr[23]) < 2009){
+                    double budget = Double.parseDouble(arr[budgetIndex]);
+                    double gross = Double.parseDouble(arr[grossIndex]);
+                    double margin = successGate(budget, gross);
 
-                //Directors
-                directors.addFactor(arr[headerIndex[0]], success, 1.0);
+                    //boolean success = true;
 
-                //Duration
-                int dur = (int) (Math.round(Double.parseDouble(arr[headerIndex[1]])/30) * 30);
-                String roundDur = String.valueOf(dur);
-                duration.addFactor(roundDur, success, 1.0);
+                    //Directors
+                    if(directors.nameExists(arr[headerIndex[0]]) == false){
+                        directors.addFactor(arr[headerIndex[0]], margin);
+                    }
+                    else{
+                        directors.addAppearance(arr[headerIndex[0]]);
+                    }
 
-                //Actors
-                int iter = 0;
-                while(iter < actor_index.length){
-                    actors.addFactor(arr[actor_index[iter]], success, 1.0);
-                    iter++;
+
+                    //Duration
+                    int dur = (int) (Math.round(Double.parseDouble(arr[headerIndex[1]])/30) * 30);
+                    String roundDur = String.valueOf(dur);
+                    if(duration.nameExists(roundDur) == false){
+                        duration.addFactor(roundDur, margin);
+                    }
+                    else{
+                        duration.addAppearance(roundDur);
+                    }
+
+                    //Actors
+                    int iter = 0;
+                    while(iter < actor_index.length){
+                        if(actors.nameExists(arr[actor_index[iter]]) == false){
+                            actors.addFactor(arr[actor_index[iter]], margin);
+                        }
+                        else{
+                            actors.addAppearance(arr[actor_index[iter]]);
+                        }
+                        iter++;
+                    }
+
+                    //Content Rating
+                    if(rating.nameExists(arr[headerIndex[3]]) == false){
+                        rating.addFactor(arr[headerIndex[3]], margin);
+                    }
+                    else{
+                        rating.addAppearance(arr[headerIndex[3]]);
+                    }
+
+
+                    //Size will vary based on csv file, this case 0-27
+                    trainingData.add(count, arr);
+                    count++;
                 }
-
-                //Content Rating
-                rating.addFactor(arr[headerIndex[3]], success, 1.0);
-
-
-                //Size will vary based on csv file, this case 0-27
-                data.add(count, arr);
-                count++;
+                else{
+                    testData.add(arr);
+                }
             }
 
 
         }
         scanner.close();
+        String n = "Tim Burton";
+        for(int i=0; i<1; i++){
+            for(String[] line : trainingData){
+                String name = line[1];
+                double budget = Double.parseDouble(line[budgetIndex]);
+                double gross = Double.parseDouble(line[grossIndex]);
+                double margin = successGate(budget, gross);
+                int dur = (int) (Math.round(Double.parseDouble(line[headerIndex[1]])/30) * 30);
+                String roundDur = String.valueOf(dur);
+//                double durationWeight = duration.getWeight(roundDur);
+//                double prediction = (.4*directorWeight*(directorApp/5) + .2*actorWeight1*(actor1App/5) + .2*actorWeight2*(actor2App/5)
+//                        + .2*actorWeight3*(actor3App/5) + .00*ratingWeight + .00*durationWeight);
+                actors.update(line[actor_index[0]], margin, budget);
+                actors.update(line[actor_index[1]], margin, budget);
+                actors.update(line[actor_index[2]], margin, budget);
+                directors.update(line[headerIndex[0]], margin, budget);
+                rating.update(line[headerIndex[3]], margin, budget);
+                duration.update(roundDur, margin, budget);
+                double actorWeight1 = actors.getWeight(line[actor_index[0]]);
+                double actorWeight2 = actors.getWeight(line[actor_index[1]]);
+                double actorWeight3 = actors.getWeight(line[actor_index[2]]);
+                double directorWeight = directors.getWeight(line[headerIndex[0]]);
+//                double actor1App = actors.getAppearances(line[actor_index[0]]);
+//                double actor2App = actors.getAppearances(line[actor_index[1]]);
+//                double actor3App = actors.getAppearances(line[actor_index[2]]);
+//                double directorApp = directors.getAppearances(line[headerIndex[0]]);
+                double ratingWeight = rating.getWeight(line[headerIndex[3]]);
+                if(name.equals(n)) {
+                    System.out.println("" + (double)Math.round(margin*1000)/1000);
+                    System.out.println("\t" + actorWeight1 + "\t" + actorWeight2 + "\t" + actorWeight3 + "\t" + directorWeight);
+                }
+            }
+        }
+        System.out.println();
+        for(String[] line : testData){
+            double margin = successGate(Double.parseDouble(line[budgetIndex]), Double.parseDouble(line[grossIndex]));
+            String name = line[1];
+            double actorWeight1 = actors.getWeight(line[actor_index[0]]);
+            double actorWeight2 = actors.getWeight(line[actor_index[1]]);
+            double actorWeight3 = actors.getWeight(line[actor_index[2]]);
+            double directorWeight = directors.getWeight(line[headerIndex[0]]);
+            double actor1App = actors.getAppearances(line[actor_index[0]]);
+            double actor2App = actors.getAppearances(line[actor_index[1]]);
+            double actor3App = actors.getAppearances(line[actor_index[2]]);
+            double directorApp = directors.getAppearances(line[headerIndex[0]]);
+            if(name.equals(n)) {
+                System.out.println("" + (double)Math.round(margin*1000)/1000);
+                System.out.println("\t" + actorWeight1 + "\t" + actorWeight2 + "\t" + actorWeight3 + "\t" + directorWeight);
+            }
+//            double prediction = (.4*directorWeight*(directorApp/5) + .2*actorWeight1*(actor1App/5) + .2*actorWeight2*(actor2App/5)
+//                    + .2*actorWeight3*(actor3App/5));
+            //System.out.println(name + " ; " + (double)Math.round(margin*1000)/1000 + " : " + (double)Math.round(prediction*1000)/1000);
+
+        }
 
 
 //        int m = 0;
@@ -77,21 +162,22 @@ public class Main {
 //            System.out.println(data.get(m)[1]);
 //            m = m+10;
 //        }
-        System.out.println(data.get(95)[1]);
-        System.out.println(data.get(3)[1]);
-        System.out.println(data.get(3)[2]);
-        System.out.println(rating.toString());
+        System.out.println(trainingData.get(95)[1]);
+        System.out.println(trainingData.get(11)[1]);
+        System.out.println(trainingData.get(3)[2]);
+        //System.out.println(actors.toString());
         //System.out.println(count);
     }
 
-    public static boolean successGate(double budget, double gross){
+    public static double successGate(double budget, double gross){
         double margin = gross/budget;
-        if(margin > 0.8){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return margin;
+//        if(margin > 0.8){
+//            return true;
+//        }
+//        else{
+//            return false;
+//        }
     }
 
     public static boolean dataGate(String[] line, int budget, int gross){
